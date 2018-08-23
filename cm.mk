a9b4f6bc112526ca3eb930f3e22b26e071362909
sp4rt4n-i17 <42655161+sp4rt4n-i17@users.noreply.github.com>
2018-08-23 21:56:18 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := GRA-L09
+PRODUCT_RELEASE_NAME := M2-801LC100
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/huawei/hwgra/device_hwgra.mk)
+$(call inherit-product, device/huawei/hwmozart/device_hwgra.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE:=hwgra
-PRODUCT_NAME:=cm_hwgra
+PRODUCT_DEVICE:=hwmozart
+PRODUCT_NAME:=cm_hwmozart
 PRODUCT_BRAND:=HUAWEI
-PRODUCT_MODEL:=HUAWEI GRA-L09
+PRODUCT_MODEL:=HUAWEI M2-801LC100
 PRODUCT_MANUFACTURER:=HUAWEI
